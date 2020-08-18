4f6e180cda9b1ab9f72aefe16a4dc0768d4a3ad6
Daniel Leuthold <daniel.leuthold@inware.ch>
2020-08-18 17:31:28 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.scss';
+import Nav from './pagecomponents/Nav';
+import Aside from './pagecomponents/Aside';
+import Main from './pagecomponents/Main';
+import Footer from './pagecomponents/Footer';
+import $ from 'jquery';
 
 function App() {
+  const [appClass, setAppClass] = useState('App hidden');
+
+  useEffect(() => {
+      $.get('/categories', {}, (res) => {
+          console.log(res);
+      });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={appClass}>
+      <Nav />
+      <Aside />
+      <Main />
+      <Footer />
     </div>
   );
 }
